b9a752b715c718981f0fe9a560ac31fab86632c2
Moritz Schlarb <mail@moritz-schlarb.de>
2010-11-20 00:54:09 +0100

    Pickling of responses now implemented! New methods exorcism() and sayByProxy() handle the more difficult commands, the bot has. History of ghostwriters is being kept.

--- testbot.py
+++ testbot.py
@@ -12,6 +12,8 @@ ircbot.py.
 """
 
 CONFIG_FILE = "testbot.cfg"
+RESPONSES_FILE = "responses.pkl"
+MAX_GHOSTS = 10
 
 from ircbot import SingleServerIRCBot
 from irclib import nm_to_n, nm_to_h, irc_lower, ip_numstr_to_quad, ip_quad_to_numstr
@@ -26,21 +28,43 @@ class TestBot(SingleServerIRCBot):
         SingleServerIRCBot.__init__(self, [(server, port)], nickname, realname)
         self.channel_list = channel_list
         self.channel = channel_list[0]
-        self.lastuser = "Nobody."
+        self.ghostwriters = [("Nobody","Nothing!")]
+        self.loadResponses()
     
-    def setResponses(self,responses):
-        self.responses = responses
+    def saveResponses(self):
+        """
+        Saves (pickles) responses to the file RESPONSES_FILE
+        """
+        import pickle
+        storage = file(RESPONSES_FILE, "wb")
+        pickle.dump(self.responses,storage,protocol=pickle.HIGHEST_PROTOCOL)
+        storage.close()
+        return
+    
+    def loadResponses(self):
+        """
+        Loads pickled responses from the file RESPONSES_FILE
+        """
+        import pickle
+        storage = file(RESPONSES_FILE, "rb")
+        self.responses = pickle.load(storage)
+        storage.close()
     
     def on_nicknameinuse(self, c, e):
         c.nick(c.get_nickname() + "_")
     
     def on_welcome(self, c, e):
+        """
+        Handles server welcome
+        """
         for chan in self.channel_list:
             c.join(chan)
         #c.privmsg(self.channel,"moschlar Hallo Papa!")
     
     def on_pubmsg(self, c, e):
-        
+        """
+        Handles public messages.
+        """
         a = e.arguments()[0]
         
         # Did someone mention my name?
@@ -57,6 +81,9 @@ class TestBot(SingleServerIRCBot):
         return
     
     def on_privmsg(self, c, e):
+        """
+        Handles private messages
+        """
         a = e.arguments()[0]
         
         cmd = a.split()
@@ -67,19 +94,65 @@ class TestBot(SingleServerIRCBot):
             
             
         elif cmd[0] == "exorcism":
-            c.privmsg(e.source(),self.lastuser)
+            try:
+                n = int(cmd[1])
+                self.exorcism(c, e, n)
+            except:
+                self.exorcism(c, e)
             
         elif cmd[0] == "keywords":
             c.privmsg(e.source(),self.responses.keys())
             
         elif (cmd[0] == "say") and (len(cmd) > 1):
-            if cmd[1].startswith("#"):
-                chan = cmd[1]
-            else:
-                chan = self.channel
-            self.lastuser = nm_to_n(e.source())
-            c.privmsg(chan,a.split(None,2)[-1])
+            self.sayByProxy(c, e, a.split(None,1)[-1])
+            
+        elif (cmd[0] == "response") and (len(cmd) > 1):
+            if cmd[1] == "list":
+                c.privmsg(e.source(),self.responses.keys())
+            elif cmd[1] == "set" and len(cmd) > 3:
+                self.responses[cmd[2]] = cmd[3]
+                c.privmsg(e.source(),"Set %s to %s" % (cmd[2],cmd[3]))
+                self.saveResponses
+            elif cmd[1] == "del" and len(cmd) > 2:
+                if self.channels[self.channel].is_voiced(nm_to_n(e.source())):
+                    c.privmsg(e.source(),"%s gelöscht" % cmd[2])
+                    del(self.responses[cmd[2]])
+                else:
+                    c.privmsg(e.source(),"Nah, des geht nit!")
+            
+        return
+    
+    def exorcism(self,c,e,*n):
+        """
+        Writes a list of the history of ghostwriters that used sayByProxy
+        """
+        if (not n) or (n[0] > len(self.ghostwriters)):
+            n = len(self.ghostwriters)
+        else:
+            n = n[0]
         
+        for (who,what) in self.ghostwriters[:n]:
+            c.privmsg(e.source(),"%s said %s" % (who,what))
+        return
+    
+    def sayByProxy(self,c,e,msg):
+        """
+        Makes the bot say something as it were his own words
+        
+        Keeps MAX_GHOSTS entries as history of ghostwriters.
+        """
+        if len(self.ghostwriters) >= MAX_GHOSTS:
+            self.ghostwriters = self.ghostwriters[1:]
+        self.ghostwriters.append((nm_to_n(e.source()),msg))
+        
+        msgsplit = msg.split(None,1)
+        if msg.startswith("#") and len(msgsplit) > 1:
+            chan = msgsplit[0]
+            msg = msgsplit[1]
+        else:
+            chan = self.channel
+        
+        c.privmsg(chan,msg)
         return
     
 def main():
@@ -95,14 +168,7 @@ def main():
     channel_list = config.get("global", "channel_list").split(",")
     print("I'm %s (%s) at %s%s" %(nickname, realname, server, channel_list))
     
-    # Make dictionary of responses
-    responses = {}
-    for i in config.items("responses"):
-        responses[i[0]] = i[1]
-    print("I respond to: %s" % responses)
-    
     testbot = TestBot(server, nickname, realname, channel_list)
-    testbot.setResponses(responses)
     testbot.start()
     
     return
